bf3c0f664f2325e7c825eef02471a1846c88e40d
CrazyAlan <xiaochuan.chen91@gmail.com>
2017-03-23 14:16:50 -0700

    finished moving pics

--- util/sortLfw.py
+++ util/sortLfw.py
@@ -12,39 +12,51 @@ import sys
 
 def main(args):
 
-	persons = os.listdir(os.path.expanduser(args.data_dir))
-	name_dic = {}
-	for person in persons:
-		person_folder=os.path.join(os.path.expanduser(args.data_dir),person)
-		if not os.path.isdir(person_folder):
-			print('not folder')
-			continue
-		# person_folder=os.path.join(os.path.expanduser(args.data_dir),person)
-		name_dic[person] = len(os.listdir(person_folder))
+    persons = os.listdir(os.path.expanduser(args.data_dir))
+    name_dic = {}
+    for person in persons:
+        person_folder=os.path.join(os.path.expanduser(args.data_dir),person)
+        if not os.path.isdir(person_folder):
+            print('not folder')
+            continue
+        # person_folder=os.path.join(os.path.expanduser(args.data_dir),person)
+        name_dic[person] = len(os.listdir(person_folder))
 
-	sorted_x = sorted(name_dic.items(), key=operator.itemgetter(1))
+    sorted_x = sorted(name_dic.items(), key=operator.itemgetter(1))
 
-	if not os.path.isdir(args.output_dir):  # Create the output directory if it doesn't exist
-                os.makedirs(args.output_dir)
+    if not os.path.isdir(args.output_dir):  # Create the output directory if it doesn't exist
+        os.makedirs(args.output_dir)
 
-	for i in range(1,args.size+1):
+    for i in range(1,args.size+1):
 
-		org=os.path.join(os.path.expanduser(args.data_dir),sorted_x[-i][0])
-		dst=os.path.join(os.path.expanduser(args.output_dir),sorted_x[-i][0])
-		shutil.copytree(org, dst)
-		print(dst)
-		# print sorted_x
+        org=os.path.join(os.path.expanduser(args.data_dir),sorted_x[-i][0])
+        dst=os.path.join(os.path.expanduser(args.output_dir),sorted_x[-i][0])
+
+        if not os.path.isdir(dst):  # Create the output directory if it doesn't exist
+            os.makedirs(dst)
+
+        img_list = os.listdir(org)
+        for j in range(args.per_imgs):
+            org_file = os.path.join(os.path.expanduser(org),img_list[j])
+            dst_file = os.path.join(os.path.expanduser(dst),img_list[j])
+            shutil.copyfile(org_file, dst_file)
+
+        print(dst)
+        # print sorted_x
 
 def parse_arguments(argv):
-	parser = argparse.ArgumentParser()
-	
-	parser.add_argument('--data_dir', type=str, default='~/remote/datasets',
-		help='Path to the data directory containing aligned LFW face patches.')
-	parser.add_argument('--output_dir', type=str, default='~/remote/datasets',
-		help='Path to the data directory containing aligned LFW face patches.')
-	parser.add_argument('--size', type=int,
-		help='Number of images to copy to store', default=10)
-	return parser.parse_args(argv)
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--data_dir', type=str, default='~/remote/datasets',
+        help='Path to the data directory containing aligned LFW face patches.')
+    parser.add_argument('--output_dir', type=str, default='~/remote/datasets',
+        help='Path to the data directory containing aligned LFW face patches.')
+    parser.add_argument('--size', type=int,
+        help='Number of images to copy to store', default=10)
+    parser.add_argument('--per_imgs', type=int,
+        help='Number of images to for each pearson', default=20)
+
+    return parser.parse_args(argv)
 
 if __name__ == '__main__':
-	main(parse_arguments(sys.argv[1:]))
+    main(parse_arguments(sys.argv[1:]))
